8a5dbf282ab19bced40ce0f9c906d8a434155706
Adalbert Mwombeni <adalbert.mwombeni@ksb.com>
2021-04-24 11:15:55 +0200

    added a file that adds all contents of a list

---
+++ addElementsOfList.py
@@ -0,0 +1,14 @@
+
+list = [1, 5, 6, 8]
+
+print("list: ", list)
+
+
+convertedArray = []
+sum = 0
+for i in list:
+    sum = sum +i
+    convertedArray.append(sum)
+
+result = convertedArray[0: len(convertedArray)-1]
+print("result: ", result)
